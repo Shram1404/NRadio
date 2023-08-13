d642708eac785c4d252307e9e958d9fa21abf44f
Олег Шрам <shram.20030414@gmail.com>
2023-08-13 17:26:35 +0300

    fix first mainpage loading

--- NRadio/Services/ActivationService.cs
+++ NRadio/Services/ActivationService.cs
@@ -11,19 +11,12 @@ using Windows.UI.Xaml.Controls;
 
 namespace NRadio.Services
 {
-    // For more information on understanding and extending activation flow see
-    // https://github.com/microsoft/TemplateStudio/blob/main/docs/UWP/activation.md
     internal class ActivationService
     {
         private readonly App app;
         private readonly Type defaultNavItem;
         private Lazy<UIElement> shell;
-
-        private object _lastActivationArgs;
-
-        private IdentityService IdentityService => Singleton<IdentityService>.Instance;
-
-        private UserDataService UserDataService => Singleton<UserDataService>.Instance;
+        private object lastActivationArgs;
 
         public ActivationService(App app, Type defaultNavItem, Lazy<UIElement> shell = null)
         {
@@ -33,12 +26,28 @@ namespace NRadio.Services
             IdentityService.LoggedIn += OnLoggedIn;
         }
 
+        private IdentityService IdentityService => Singleton<IdentityService>.Instance;
+        private UserDataService UserDataService => Singleton<UserDataService>.Instance;
+
+        public async Task RedirectLoginPageAsync()
+        {
+            var frame = new Frame();
+            NavigationService.Frame = frame;
+            Window.Current.Content = frame;
+            await LanguageSelectorService.SetRequestedLanguageAsync();
+            await ThemeSelectorService.SetRequestedThemeAsync();
+            NavigationService.Navigate<Views.LogInPage>();
+        }
+
+        public void SetShell(Lazy<UIElement> shell)
+        {
+            this.shell = shell;
+        }
+
         public async Task ActivateAsync(object activationArgs)
         {
             if (IsInteractive(activationArgs))
             {
-                // Initialize services that you need before app activation
-                // take into account that the splash screen is shown while this code runs.
                 await InitializeAsync();
                 UserDataService.Initialize();
                 IdentityService.InitializeWithAadAndPersonalMsAccounts();
@@ -48,23 +57,18 @@ namespace NRadio.Services
                     await RedirectLoginPageAsync();
                 }
 
-                // Do not repeat app initialization when the Window already has content,
-                // just ensure that the window is active
                 if (Window.Current.Content == null)
                 {
-                    // Create a Shell or Frame to act as the navigation context
                     Window.Current.Content = shell?.Value ?? new Frame();
                 }
             }
 
-            // Depending on activationArgs one of ActivationHandlers or DefaultActivationHandler
-            // will navigate to the first page
             if (IdentityService.IsLoggedIn())
             {
                 await HandleActivationAsync(activationArgs);
             }
 
-            _lastActivationArgs = activationArgs;
+            lastActivationArgs = activationArgs;
 
             if (IsInteractive(activationArgs))
             {
@@ -74,10 +78,7 @@ namespace NRadio.Services
                     await Singleton<SuspendAndResumeService>.Instance.RestoreSuspendAndResumeData();
                 }
 
-                // Ensure the current window is active
                 Window.Current.Activate();
-
-                // Tasks after activation
                 await StartupAsync();
             }
         }
@@ -97,7 +98,7 @@ namespace NRadio.Services
 
             await LanguageSelectorService.SetRequestedLanguageAsync();
             await ThemeSelectorService.SetRequestedThemeAsync();
-            await HandleActivationAsync(_lastActivationArgs);
+            await HandleActivationAsync(lastActivationArgs);
         }
 
         private async Task InitializeAsync()
@@ -144,21 +145,6 @@ namespace NRadio.Services
         private bool IsInteractive(object args)
         {
             return args is IActivatedEventArgs;
-        }
-
-        public async Task RedirectLoginPageAsync()
-        {
-            var frame = new Frame();
-            NavigationService.Frame = frame;
-            Window.Current.Content = frame;
-            await LanguageSelectorService.SetRequestedLanguageAsync();
-            await ThemeSelectorService.SetRequestedThemeAsync();
-            NavigationService.Navigate<Views.LogInPage>();
-        }
-
-        public void SetShell(Lazy<UIElement> shell)
-        {
-            this.shell = shell;
         }
     }
 }
